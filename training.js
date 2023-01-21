29752921ca6e4ed8ef124e4487a4054e2065214d
Yaroslav <vivchariukyaroslav@gmail.com>
2023-01-21 16:28:04 +0300

    +

---
+++ training.js
@@ -0,0 +1,70 @@
+// Напишіть через світч пошук автора мови програмування
+// Пишемо назву мови у шаблонному рядку отримує відповідь: мова та автор
+// PHP Расмус Лердорф
+// C# група інженерів компанії Microsoft під керівництвом Андерса Хейлсберга та Скотта Вільтаумота
+// Swift Кріс Латтнер
+// JS Брендан Ейх
+// Java Джеймс Гослінг
+// Python Гвідо ван Россум
+
+
+// const programName = prompt('Введіть мову програмування')?.toLowerCase();
+
+// switch (programName) {
+//     case 'php':
+//         console.log('PHP Расмус Лердорф');
+//         break;
+//     case 'c#':
+//         console.log(
+//             'C# група інженерів компанії Microsoft під керівництвом Андерса Хейлсберга та Скотта Вільтаумота'
+//         );
+//         break;
+//     case 'swift':
+//         console.log('Swift Кріс Латтнер');
+//         break;
+//     case 'js':
+//         console.log('JS Брендан Ейх');
+//         break;
+//     case 'java':
+//         console.log('Java Джеймс Гослінг');
+//         break;
+//     case 'python':
+//         console.log('Python Гвідо ван Россум');
+//         break;
+//     default:
+//         console.log('не знаю такої мови');
+// }
+
+
+
+// --------
+
+// Напишіть через світч калькулятор
+// Потрібно ввести перше число в prompt, потім математичну операцію
+// Тоді ввести наступне число
+
+// const num = +prompt('Enter number');
+// const reiter = prompt('Enter reiteration');
+// const num2 = +prompt('Enter second number');
+
+// let result;
+
+// switch (reiter) {
+//     case '+':
+//         result = num + num2;
+//         break;
+//     case '-':
+//         result = num - num2;
+//         break;
+//     case '*':
+//         result = num * num2;
+//         break;
+//     case '/':
+//         result = num / num2;
+//         break;
+    
+//     default:
+//         console.log('It/s not number');
+// }
+
+// console.log(result);
